a604406b03b06b40702fbe84dc8f31f1a4f07260
Mr.orange <309871271@qq.com>
2015-11-30 17:46:06 +0800

    jquery ui

--- admin/user_edit.php
+++ admin/user_edit.php
@@ -135,7 +135,7 @@ require_once '_footer.php'; ?>
             $("#msg-error").hide(100);
         })
         $("#end_date").datetimepicker({ 
-            dateFormat: "yyyy-MM-dd hh:mm:ss",
+            format: "yyyy-MM-dd hh:mm:ss",
             changeMonth: true,
             changeYear: true,
             pickTime: true,
