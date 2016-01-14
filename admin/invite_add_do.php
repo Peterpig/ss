14f246ea7ba363f7dc8c103f3dec1b80f8800cc2
Mr.orange <309871271@qq.com>
2016-01-14 17:48:58 +0800

    fix bug

--- admin/invite_add_do.php
+++ admin/invite_add_do.php
@@ -8,8 +8,9 @@ $num  = $_POST['code_num'];
 $c = new \Ss\User\InviteCode();
 $c->AddCode($sub,$type,$num);
 echo '
+<script src="http://anyb.applinzi.com/asset/layer.js"></script>
 <script type="text/javascript" charset="utf-8">
-    layer.alert("添加成功!")
-    window.location="invite.php"
+    layer.alert("添加成功!");
+    window.location="invite.php";
 </script>
 ';
